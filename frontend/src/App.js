540e4675e9c5cf87a7f3c5d4f67f5f05d94679f1
Paul Nyishar <paulnyishar@gmail.com>
2022-08-06 05:10:47 +0100

    Reat-app creation

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List of Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
